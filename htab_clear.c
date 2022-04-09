0af29e7219948d07346cfc821df75dedc968bf4f
S1ro1 <matej.sirovatka@gmail.com>
2022-04-09 18:39:22 +0200

    Add first modules

---
+++ htab_clear.c
@@ -0,0 +1,23 @@
+#include "hash_tab_struct.h"
+#include "htab.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+void htab_clear(htab_t *t) {
+  struct htab_item *current;
+  struct htab_item *to_delete;
+
+  for (size_t idx = 0; idx < t->arr_size; idx++) {
+    current = t->arr_ptr[idx];
+    while (current != NULL) {
+      to_delete = current;
+      current = current->next;
+
+      free(to_delete);
+    }
+    t->arr_ptr[idx] = NULL;
+  }
+
+  t->arr_size = 0;
+}
---
+++ htab_find.c
@@ -0,0 +1,27 @@
+#include "hash_tab_struct.h"
+#include "htab.h"
+
+#include <string.h>
+
+htab_pair_t *htab_find(htab_t *t, htab_key_t key) {
+  if (!t) {
+    return NULL;
+  }
+
+  size_t hash = htab_hash_function(key);
+  size_t index = hash % t->arr_size;
+
+  htab_pair_t *found_pair = NULL;
+
+  struct htab_item *current = t->arr_ptr[index];
+
+  while (current != NULL) {
+    if (strcmp(current->pair.key, key) == 0) {
+      found_pair = &current->pair;
+      break;
+    }
+    current = current->next;
+  }
+
+  return found_pair;
+}
