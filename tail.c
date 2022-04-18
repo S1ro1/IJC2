878cc78cff89c4a5b5d870f3259f919f7c062ee8
S1ro1 <matej.sirovatka@gmail.com>
2022-04-18 22:49:11 +0200

    Add new tail implementation

--- tail.c
+++ tail.c
@@ -4,51 +4,156 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_BUFFER_LEN 8
-#define MAX_STR_LEN 6
+#define MAX_STR_LEN 7 // line length with \n but without \0
 
-// TODO split functionality into separate functions
-
-int main(int argc, char *argv[]) {
-  FILE *source;
-  if (argc > 2) {
-    fprintf(stderr, "Too many arguments provided\n");
-    exit(1);
-  } else if (argc < 2) {
-    source = stdin;
-  } else {
-    source = fopen(argv[1], "r");
+int tail(FILE *stream, long n_lines) {
+  if (!n_lines) {
+    return 0;
   }
 
-  char lines[10][MAX_BUFFER_LEN] = {0};
-  int i = 0;
-  char c;
+  printf("Num of lines is: %lu\n", n_lines);
 
-  // TODO redo for changeable n (number of lines to be printed)
-  while (fgets(lines[i % 10], MAX_BUFFER_LEN, source) != NULL) {
-    int len = strlen(lines[i % 10]);
-    if (lines[i % 10][len - 1] != '\n') {
-      while ((c = getc(source)) != '\n') {
+  char **lines = calloc(n_lines, sizeof(char *));
+
+  if (!lines) {
+    fprintf(stderr, "Couldn't allocate memory!\n");
+    return 1;
+  }
+
+  for (int i = 0; i < n_lines; i++) {
+    lines[i] = calloc(MAX_STR_LEN + 1, sizeof(char));
+  }
+
+  int current_line = 0;
+  char c;
+  int len;
+
+  while (fgets(lines[current_line % n_lines], MAX_STR_LEN, stream)) {
+    len = strlen(lines[current_line % n_lines]);
+
+    if (lines[current_line % n_lines][len - 1] != '\n') {
+      while ((c = getc(stream)) != '\n') {
         ;
       }
     }
-    i++;
+    current_line++;
   }
 
   bool found_too_long = false;
-
   int curr = 0;
-  for (int j = i % 10; curr < 10; j++, curr++) {
-    int len = strlen(lines[j % 10]);
 
-    if (lines[j][len - 1] != '\n') {
-      lines[j % 10][len - 1] = '\n';
-      if (found_too_long == false) {
-        fprintf(stderr, "Line exceeds the maximal length\n");
+  for (int j = current_line % n_lines; curr < n_lines; j++, curr++) {
+    len = strlen(lines[j % n_lines]);
+
+    if (len != 0 && lines[j % n_lines][len - 1] != '\n') {
+      lines[j % n_lines][len] = '\n';
+
+      if (!found_too_long) {
+        fprintf(stderr, "Line exceeds the maximal length!\n");
       }
       found_too_long = true;
     }
-    printf("%s", lines[j % 10]);
+    printf("%s", lines[j % n_lines]);
   }
+
+  for (int i = 0; i < n_lines; i++) {
+    free(lines[i]);
+  }
+
+  free(lines);
+
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  long num_lines = 10;
+  FILE *input = stdin;
+  char *str_part = NULL;
+
+  switch (argc) {
+  case 1:
+    break;
+
+  case 2:
+    input = fopen(argv[1], "r");
+    if (!input) {
+      fprintf(stderr, "Couldn't open file!\n");
+      return 1;
+    }
+    break;
+
+  case 3:
+    if (strcmp(argv[1], "-n")) {
+      fprintf(stderr, "Wrong command line arguments!\n");
+      return 1;
+    } else {
+      if (argv[2][0] == '+' || argv[2][0] == '-') {
+        fprintf(stderr, "Only unsigned values are supported!\n");
+        return 1;
+      }
+      num_lines = strtol(argv[2], &str_part, 10);
+
+      if (*str_part) {
+        fprintf(stderr, "Wrong number of lines provided!\n");
+        return 1;
+      }
+    }
+    break;
+
+  case 4:
+    if (!strcmp(argv[1], "-n")) {
+      if (argv[2][0] == '+' || argv[2][0] == '-') {
+        fprintf(stderr, "Only unsigned values are supported!\n");
+        return 1;
+      }
+      num_lines = strtol(argv[2], &str_part, 10);
+
+      if (*str_part) {
+        fprintf(stderr, "Wrong number of lines provided!\n");
+        return 1;
+      }
+
+      input = fopen(argv[3], "r");
+      if (!input) {
+        fprintf(stderr, "Couldn't open file!\n");
+        return 1;
+      }
+    } else if (!strcmp(argv[2], "-n")) {
+      if (argv[3][0] == '+' || argv[3][0] == '-') {
+        fprintf(stderr, "Only unsigned values are supported!\n");
+        return 1;
+      }
+      num_lines = strtol(argv[3], &str_part, 10);
+
+      if (*str_part) {
+        fprintf(stderr, "Wrong number of lines provided!\n");
+        return 1;
+      }
+
+      input = fopen(argv[1], "r");
+      if (!input) {
+        fprintf(stderr, "Couldn't open file!\n");
+        return 1;
+      }
+    } else {
+      fprintf(stderr, "Wrong command line arguments!\n");
+      return 1;
+    }
+    break;
+  default:
+    fprintf(stderr, "Wrong command line arguments!\n");
+    return 1;
+    break;
+  }
+
+  int ret = tail(input, num_lines);
+
+  if (ret) {
+    fprintf(stderr, "Error printing lines!\n");
+    fclose(input);
+    return 1;
+  }
+
+  fclose(input);
   return 0;
 }
